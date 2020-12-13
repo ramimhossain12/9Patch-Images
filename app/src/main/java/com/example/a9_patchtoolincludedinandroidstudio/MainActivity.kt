2cfb34876056ba0b2cea01dc32be2a16d196a610
ramimhossain12 <ramimhossain6242@gmail.com>
2020-12-14 00:17:55 +0600

    Language change

---
+++ app/src/main/java/com/example/a9_patchtoolincludedinandroidstudio/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a9_patchtoolincludedinandroidstudio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a9_patchtoolincludedinandroidstudio.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
